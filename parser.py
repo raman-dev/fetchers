112577f983f310d1c55153bdce5e17ab7b18f48f
raman-dev <ramandeep.dev582@gmail.com>
2026-06-15 23:54:15 -0400

    some parser stuff

---
+++ parser.py
@@ -0,0 +1,32 @@
+from abc import ABC,abstractmethod
+from enum import Enum
+
+class Parser(ABC):
+    @abstractmethod
+    def parse(self):
+        pass
+    
+
+class TapologyParser(Parser):
+    class ParseType(Enum):
+        PARSE_RESULTS = "parse_results"
+        PARSE_MATCHUPS = "parse_matchups"
+
+    def parse(self, source ,parseType: ParseType):
+        if parseType == None:
+            raise ValueError("parseType not specified")
+        if type(parseType) != type(self.ParseType.PARSE_RESULTS):
+            raise TypeError("parseType is not of type TapologyParser.ParseType")
+        
+        match parseType:
+            case self.ParseType.PARSE_RESULTS:
+                return self.parse_results(source)
+            case self.ParseType.PARSE_MATCHUPS:
+                return self.parse_matchups(source)
+    
+    def parse_results(self, source):
+        return "Parsing results from Tapology"
+
+    def parse_matchups(self, source):
+        return "Parsing matchups from Tapology"
+    
